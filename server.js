fa1b3868aa4fe2020cf07e699a7ee76e60fc4acc
Nitish9909 <nitishtiwary9909@gmail.com>
2023-04-09 23:02:57 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,55 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('DB Connection Successfull');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  ratimg: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Camper 2',
+  price: 2345,
+});
+
+testTour
+  .save()
+  .then(() => {
+    console.log('Data Saved in DB');
+  })
+  .catch((err) => {
+    console.log('Error', err);
+  });
 
 const port = process.env.PORT || 3000;
 
